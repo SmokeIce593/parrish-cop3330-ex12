c073471a657c3ed1b396be4de617ddf6135013cb
SmokeIce593 <37118809+SmokeIce593@users.noreply.github.com>
2021-09-12 17:47:39 -0400

    Edit for rounding up

--- src/main/java/org/example/Interestcalculator.java
+++ src/main/java/org/example/Interestcalculator.java
@@ -5,6 +5,7 @@
 
 package org.example;
 import java.util.Scanner;
+import java.lang.Math;
 
 
 public class Interestcalculator {
@@ -15,19 +16,19 @@ public class Interestcalculator {
         System.out.print("Enter the principal:");
         String principalString = input.nextLine();
 
-        float principal = Float.parseFloat(principalString);
+        double principal = Double.parseDouble(principalString);
 
         System.out.print("Enter the rate of interest:");
         String rateString = input.nextLine();
 
-        float rate = Float.parseFloat(rateString);
+        double rate = Double.parseDouble(rateString);
 
         System.out.print("Enter the number of years:");
         String yearsString = input.nextLine();
 
-        float years = Float.parseFloat(yearsString);
+        double years = Double.parseDouble(yearsString);
 
-        float total = principal * (1 + rate/100 * years);
+        double total = Math.ceil((principal * (1 + rate/100 * years)) * 100)/100;
 
         System.out.printf("After %.2f years at %.2f percent, the investment will be worth $%.2f", years, rate, total);
 
